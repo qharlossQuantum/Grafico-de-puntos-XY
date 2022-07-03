220347ccdde1c30ec64db3a43b781f72762eeade
qharlossQuantum <107539898+qharlossQuantum@users.noreply.github.com>
2022-07-02 20:23:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 print("ingresa un valor para\n")
 m=float(input("m="))
 
-plt.plot(1/m,m**0.5,marker ="o")
+plt.plot(5/m,m**0.5,marker ="o")
 
 m=m+4
 plt.plot(0.5*m,m**0.5,marker ="o")
